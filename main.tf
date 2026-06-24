9b4682a0ef927c82e1ee2ba8830f42580e05d0f1
putlurubharathreddy2002-dev <putlurubharathreddy2002@gmail.com>
2026-06-24 14:59:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -211,5 +211,12 @@ resource "azurerm_linux_virtual_machine" "vm" {
     storage_account_type = "Standard_LRS"
   }
 }
+resource "azurerm_container_registry" "acr" {
+  name                = "schoolappacr2026"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
 
+  sku           = "Basic"
+  admin_enabled = false
+}
 
